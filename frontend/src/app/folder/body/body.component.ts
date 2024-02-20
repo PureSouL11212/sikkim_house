1a38ab140687bd0f2ef31698c9d7800f4bfcc2a8
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2024-02-20 11:52:48 +0530

    ssss

--- frontend/src/app/folder/body/body.component.ts
+++ frontend/src/app/folder/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-body',
@@ -8,15 +8,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
-  
   availableRooms = [
-    { image: 'assets/image/imae2.jpeg', date: '2024-02-19', day: 'Monday' },
+    { image: 'assets/image/imae2.jpeg', date: '2024-02-19', day: 'Monday' , RoomAvailable:'10' },
     { image: 'assets/image/image3.webp', date: '2024-02-20', day: 'Tuesday' },
     { image: 'assets/image/image4.jpeg', date: '2024-02-21', day: 'Wednesday' },
     { image: 'assets/image/room1.jpg', date: '2024-02-22', day: 'Thursday' },
     { image: 'assets/image/image5.webp', date: '2024-02-23', day: 'Friday' }
   ];
-
   proceed() {
     // Your proceed function logic here
   }
